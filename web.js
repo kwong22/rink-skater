7e3c1bfd32a5a9cd7fd1e4431f35afc6f02628d5
Kelvin Wong <kwong691@gmail.com>
2013-11-04 23:42:06 +0000

    Clean up

--- web.js
+++ web.js
@@ -7,17 +7,6 @@ app.get('/', function(request, response) {
     response.send(fs.readFileSync(htmlFile).toString());
 });
 
-/*
-fs.readFile('index.html', 'utf8', function(err, data) {
-    if (err) { return console.log(err); }
-
-    app.get('/', function(request, response) {
-	response.send(data);
-    });
-
-});
-*/
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
