01e8e98dbcc016b56afe0ed0013dcd972798b88d
Kelvin Wong <kwong691@gmail.com>
2013-11-17 16:55:43 +0000

    Created a more robust input system

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,13 @@
 // Create the canvas
-//var canvasList = document.getElementsByTagName("canvas");
-//var canvas = canvasList[0];
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d"); // use this context to draw on the canvas
 canvas.width = 480;
 canvas.height = 224;
 var wrapper = document.getElementsByClassName("game")[0];
-//document.body.appendChild(canvas);
 wrapper.appendChild(canvas);
-// Images
-var testReady = false;
-var testImage = new Image();
-testImage.onload = function() {
-    testReady = true;
-};
 
 // Initial variables
+var fps = 60;
 var mapDim = 7;
 var map = [];
 var blockSize = 32;
@@ -23,31 +15,35 @@ var playerSize = 16;
 var numMoves;
 var canMove;
 
-function loadMap() {
+function loadMap(mapId) {
     map.length = mapDim;
     for (var i = 0; i < mapDim; i++) {
 	map[i] = initArray(mapDim, 0);
     }
     
     // Use info from the loaded map
-    map[0][0] = 1;
-    map[1][0] = 1;
-    map[5][0] = 1;
-    map[6][0] = 1;
+    switch(mapId) {
+    case 1:
+	map[0][0] = 1;
+	map[1][0] = 1;
+	map[5][0] = 1;
+	map[6][0] = 1;
 
-    map[0][1] = 1;
-    map[6][1] = 1;
+	map[0][1] = 1;
+	map[6][1] = 1;
 
-    map[0][5] = 1;
-    map[6][5] = 1;
+	map[0][5] = 1;
+	map[6][5] = 1;
 
-    map[0][6] = 1;
-    map[1][6] = 1;
-    map[5][6] = 1;
-    map[6][6] = 1;
+	map[0][6] = 1;
+	map[1][6] = 1;
+	map[5][6] = 1;
+	map[6][6] = 1;
 
-    win.x = 3;
-    win.y = 3;
+	win.x = 3;
+	win.y = 3;
+	break;
+    }
 
     player.x = 0;
     player.y = 0;
@@ -75,51 +71,60 @@ var win = {
 
 
 // Handle keyboard input
-var keysUp = {};
+var keysUp = [];
+var keysUsed = [32, 37, 38, 39, 40];
+
+addEventListener("keydown", function(e) {
+    for (var i = 0; i < keysUsed.length; i++) {
+	if (e.keyCode == keysUsed[i]) { // Don't want to affect other keys like F5
+	    e.preventDefault(); // To prevent scrolling with arrow keys and spacebar
+	}
+    }
+}, false);
 
 addEventListener("keyup", function(e) {
     keysUp[e.keyCode] = true;
-    e.preventDefault();
+    // e.preventDefault(); // This does nothing with keyup event
 }, false);
 
 // Reset
 function reset() {
     numMoves = 0;
-    loadMap();
+    loadMap(1);
     canMove = true;
-    //clearInterval(mainInt);
-    //mainInt = setInterval(main, 1);
+    clearKeys(); // To get rid of any leftover input from last game
+}
+
+function clearKeys() { // No longer prone to multiple input
+    for (var i = 0; i < keysUsed.length; i++) {
+	delete keysUp[keysUsed[i]];
+    }
 }
 
 function update() {
     if (canMove) {
 	if (38 in keysUp) { // Up arrow key
 	    checkSides(0, -1);
-	    delete keysUp[38];
+	    clearKeys();
 	}
 	if (40 in keysUp) { // Down arrow key
 	    checkSides(0, 1);
-	    delete keysUp[40];
+	    clearKeys();
 	}
 	if (37 in keysUp) { // Left arrow key
 	    checkSides(-1, 0);
-	    delete keysUp[37];
+	    clearKeys();
 	}
 	if (39 in keysUp) { // Right arrow key
 	    checkSides(1, 0);
-	    delete keysUp[39];
+	    clearKeys();
 	}
     }
 
-    /*if (32 in keysUp) { // Hit the spacebar to reset the game
-	reset();
-    }*/
-
-    if (gameOver()) {
-	var winText = "Congratulations! You won in " + numMoves + " moves.";
-	ctx.fillStyle = "white";
-	ctx.fillText(winText, 224 + 4, 64);
-	canMove = false;
+    if (32 in keysUp) { // Hit the spacebar to reset the game
+	stopGame();
+	startGame();
+	clearKeys();
     }
 }
 
@@ -201,7 +206,7 @@ function render() {
     ctx.fillStyle = "rgb(180, 180, 0)";
     ctx.fillRect(player.x * blockSize + offset, player.y * blockSize + offset, playerSize, playerSize);
 
-    // Win
+    // Win spot
     var offset2 = (blockSize - 8) / 2;
     ctx.fillStyle = "rgb(180, 0, 0)";
     ctx.fillRect(win.x * blockSize + offset2, win.y * blockSize + offset2, 8, 8);
@@ -210,7 +215,12 @@ function render() {
     var movesText = "Moves: " + numMoves;
     ctx.fillStyle = "white";
     ctx.fillText(movesText, 224 + 4, 16);
-
+    
+    // Checks for game over here since render comes after update
+    if (gameOver()) {
+	stopGame();
+	drawWinMessage();
+    }
 }
 
 function drawMap() {
@@ -231,16 +241,27 @@ function gameOver() {
     return player.x == win.x && player.y == win.y;
 }
 
-function main() {
-    //var now = Date.now();
-    //var delta = now - then;
-
+function run() {
     update();
     render();
-
-    //then = now;
 }
 
-reset();
-//var then = Date.now();
-var mainInt = setInterval(main, 1);
+var intervalId;
+
+function startGame() {
+    reset();
+    if (typeof intervalId == "undefined") intervalId = setInterval(run, 1000 / fps);
+}
+
+function stopGame() {
+    canMove = false;
+    //clearInterval(intervalId); // This is for ending the game, no more loops
+}
+
+function drawWinMessage() {
+    var winText = "Congratulations! You won in " + numMoves + " moves.";
+    ctx.fillStyle = "white";
+    ctx.fillText(winText, 224 + 4, 32);
+}
+
+startGame();
